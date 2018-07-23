871b29b61bb4d336f827ae1e97f5e03b04421f29
Asutosh Satapathy <asa@ase.ch>
2018-07-23 13:09:39 +0200

    Removed redundant files

--- tests/testthat/test-planets.R
+++ tests/testthat/test-planets.R
@@ -1,11 +1,11 @@
 context('functions')
 
 test_that('We have 9 planets', {
-  expect_equal(planetnumbers(), 9)
+  expect_equal(solar.system::planetnumbers(), 9)
 })
 
-# test_that('We have all the planet names in order', {
-#   outdat <- planetnames()
-#   dat <- planets <- c("Mercury","Venus", "Earth", "Mars", "Jupiter","Saturn", "Uranus", "Neptune", "Pluto")
-#   expect_identical(dat, outdat)
-# })
+test_that('We have all the planet names in order', {
+  outdat <- solar.system::planetnames()
+  dat <- planets <- c("Mercury","Venus", "Earth", "Mars", "Jupiter","Saturn", "Uranus", "Neptune", "Pluto")
+  expect_identical(dat, outdat)
+})
